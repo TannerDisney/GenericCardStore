34f3a8d58b5f842a90050d937546ca1c3880de8d
Tanner Disney <disneytanner@gmail.com>
2024-04-26 14:47:30 -0700

    Add project files.

---
+++ GenericCardStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GenericCardStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
